f75684b7a70737debc1bf7bf9feb48aff7c9fdbb
TheJisus28 <jesus.carrascalh@gmail.com>
2025-11-04 17:51:31 -0500

    init: mini api lab

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
