df8b579885c71fe5a38e80956411b7dd6cf1e4a8
TheJisus28 <jesus.carrascalh@gmail.com>
2025-11-04 18:11:12 -0500

    ci test

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
